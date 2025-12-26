771c9401e2ec233cd19b9bebc65a43216762aa8f
skxcv312 <skxcv322@google.com>
2025-12-26 22:33:40 +0900

    first commit

---
+++ src/main/java/org/zerock/puppyrun/PuppyRunApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.puppyrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppyRunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/puppyrun/PuppyRunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.puppyrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
