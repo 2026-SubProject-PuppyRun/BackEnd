dc43e581c208e8450339ef568d3a84a9feb82164
skxcv312 <skxcv322@google.com>
2026-01-02 01:32:33 +0900

    feat: JPA Auditing 및 스케줄링 설정 추가
    - BaseTimeEntity 추가를 통한 엔티티 생성/수정 시간 자동화
    - JpaAuditingConfig 설정을 통한 Auditing 활성화
    - PuppyRunApplication에 @EnableScheduling 어노테이션 추가

--- src/main/java/org/zerock/puppyrun/PuppyRunApplication.java
+++ src/main/java/org/zerock/puppyrun/PuppyRunApplication.java
@@ -2,7 +2,9 @@ package org.zerock.puppyrun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PuppyRunApplication {
 
