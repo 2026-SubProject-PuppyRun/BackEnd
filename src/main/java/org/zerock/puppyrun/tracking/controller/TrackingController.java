d24ad6b5d29a58116451a31502f4df0c7a69f129
skxcv312 <skxcv322@google.com>
2026-02-20 13:38:27 +0900

    feat: TrackingController 산책 기록 API 구현 (#9)
    - 산책 기록 저장
      - @Valid를 사용한 요청 데이터(ResistedTrackingRequest) 유효성 검증 적용
    
    - 내 산책 기록 리스트 조회
      - 로그인한 사용자의 전체 산책 기록 조회
    
    - 산책 기록 상세 조회
      - 특정 산책 기록의 상세 정보 조회
    
    - @AuthenticationPrincipal을 활용하여 인증된 사용자 정보 바인딩 처리

---
+++ src/main/java/org/zerock/puppyrun/tracking/controller/TrackingController.java
@@ -0,0 +1,54 @@
+package org.zerock.puppyrun.tracking.controller;
+
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.puppyrun.common.auth.security.UserPrincipal;
+import org.zerock.puppyrun.tracking.controller.request.ResistedTrackingRequest;
+import org.zerock.puppyrun.tracking.controller.response.MainTrackingResponse;
+import org.zerock.puppyrun.tracking.controller.response.TrackingDetailResponse;
+import org.zerock.puppyrun.tracking.service.TrackingService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tracking")
+public class TrackingController {
+    private final TrackingService trackingService;
+
+    // 산책 저장
+    @PostMapping("")
+    public ResponseEntity<String> saveTracking(@Valid @RequestBody ResistedTrackingRequest request,
+                                               @AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        trackingService.saveTracking(userPrincipal.id(), request);
+
+        return ResponseEntity.ok("산책 저장 완료");
+    }
+
+    // 산책 기록 조회
+    @GetMapping("")
+    public ResponseEntity<MainTrackingResponse> getTrackingList(
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        MainTrackingResponse response = trackingService.getTrackingListResponse(userPrincipal.id());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{trackingId}")
+    public ResponseEntity<TrackingDetailResponse> getTracking(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @PathVariable UUID trackingId) {
+
+        TrackingDetailResponse response = trackingService.getTrackingResponse(userPrincipal.id(), trackingId);
+
+        return ResponseEntity.ok(response);
+    }
+
+}
