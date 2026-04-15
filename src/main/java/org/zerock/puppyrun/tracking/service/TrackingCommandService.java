badf4f9721dc7efc89694707b614c7e7638c7222
skxcv312 <skxcv322@google.com>
2026-04-15 16:30:47 +0900

    feat: 산책 등록시 산책한 강아지를 등록할 수 있는 로직 추가 (#9)

--- src/main/java/org/zerock/puppyrun/tracking/service/TrackingCommandService.java
+++ src/main/java/org/zerock/puppyrun/tracking/service/TrackingCommandService.java
@@ -7,6 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.puppyrun.common.exception.ResourceNotFoundException;
+import org.zerock.puppyrun.common.exception.UserForbiddenException;
+import org.zerock.puppyrun.pet.entity.Pet;
+import org.zerock.puppyrun.tracking.entity.PetTracking;
+import org.zerock.puppyrun.tracking.repository.PetTrackingRepository;
 import org.zerock.puppyrun.tracking.util.PaceConverter;
 import org.zerock.puppyrun.diary.entity.Diary;
 import org.zerock.puppyrun.diary.repository.DiaryRepository;
@@ -32,7 +37,7 @@ public class TrackingCommandService {
     private final DiaryRepository diaryRepository;
     private final TrackingRepository trackingRepository;
     private final MemberRepository memberRepository;
-
+    private final PetTrackingRepository petTrackingRepository;
     private final PetRepository petRepository;
 
     /**
@@ -66,6 +71,8 @@ public class TrackingCommandService {
 
         Tracking savedTracking = trackingRepository.save(tracking);
 
+        // 산책과 강아지 매핑
+        saveTrackingWithPets(member.getId(), request.petIdList(), savedTracking);
     }
 
     /**
@@ -110,5 +117,29 @@ public class TrackingCommandService {
         diaryRepository.findByTrackingId(trackingId)
                 .ifPresent(Diary::unsetTracking);
         trackingRepository.delete(tracking);
+    }
+
+    private void saveTrackingWithPets(UUID memberId, List<UUID> petIdList, Tracking savedTracking) {
+        List<Pet> petList = petRepository.findAllById(petIdList);
+
+        // 요청한 ID 리스트 개수와 DB에서 찾은 개수가 다르면 예외 발생 (존재하지 않는 펫 ID 포함됨)
+        if (petList.size() != petIdList.size()) {
+            throw new ResourceNotFoundException("요청한 강아지 중 존재하지 않는 강아지가 포함되어 있습니다.");
+        }
+
+        // 조회된 모든 강아지가 현재 로그인한 유저의 강아지가 맞는지 소유권 확인
+        boolean hasNotOwnedPet = petList.stream()
+                .anyMatch(pet -> pet.isNotOwner(memberId)); // Pet 엔티티의 isNotOwner 활용
+
+        if (hasNotOwnedPet) {
+            throw new UserForbiddenException("본인의 소유가 아닌 강아지는 산책에 등록할 수 없습니다.");
+        }
+
+        List<PetTracking> petTrackingList = petList.stream()
+                .map(pet -> new PetTracking(pet, savedTracking))
+                .toList();
+
+        petTrackingRepository.saveAll(petTrackingList);
+
     }
 }
